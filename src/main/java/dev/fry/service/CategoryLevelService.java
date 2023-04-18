1af33fb4cb74fd4b94586ae1ddeb45b67f66fe36
fery_dirgan <fery_dirgan@yahoo.com>
2023-04-18 09:12:52 +0700

    remove unused import

--- src/main/java/dev/fry/service/CategoryLevelService.java
+++ src/main/java/dev/fry/service/CategoryLevelService.java
@@ -5,7 +5,6 @@ import dev.fry.model.CategoryLevel;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
