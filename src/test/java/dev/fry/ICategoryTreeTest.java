5dd1fe73f4aafdac24726235d796c189c52cf964
fery_dirgan <fery_dirgan@yahoo.com>
2023-04-18 09:10:34 +0700

    add unsorted category mock data

--- src/test/java/dev/fry/ICategoryTreeTest.java
+++ src/test/java/dev/fry/ICategoryTreeTest.java
@@ -22,6 +22,21 @@ public interface ICategoryTreeTest {
         new Category(13, "Modems", 11, 1)
     );
 
+    static List<Category> UNSORTED_CATEGORIES_MOCK = List.of(
+            new Category(2, "All-in-One PCs",  1, 0),
+            new Category(4, "Laptops", 1, 2),
+            new Category(7, "Tablets", 1, 5),
+            new Category(9, "Business PCs", 3, 1),
+            new Category(11, "Networking", 0, 1),
+            new Category(13, "Modems", 11, 1),
+            new Category(1, "Computers & Tablets",  0, 0),
+            new Category(3, "Desktop PCs", 1, 1),
+            new Category(5, "Servers", 1, 3),
+            new Category(8, "Gamming PCs", 3, 0),
+            new Category(10, "Workstations", 3, 2),
+            new Category(12, "Firewalls", 11, 0)
+    );
+
     static String EXPECTED_RESULT = (
         "+ Computers & Tablets\n" +
         "  + All-in-One PCs\n" +
