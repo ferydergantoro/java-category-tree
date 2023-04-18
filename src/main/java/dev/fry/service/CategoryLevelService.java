ee21df19c8f28c989e097637b6e78e0999650e42
fery_dirgan <fery_dirgan@yahoo.com>
2023-04-18 09:12:01 +0700

    Fixing : Handle null or empty

--- src/main/java/dev/fry/service/CategoryLevelService.java
+++ src/main/java/dev/fry/service/CategoryLevelService.java
@@ -2,16 +2,31 @@ package dev.fry.service;
 
 import dev.fry.model.Category;
 import dev.fry.model.CategoryLevel;
+import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("all")
 public class CategoryLevelService {
-    public static List<CategoryLevel> buildCategoryLevel(List<Category> categories, long parentId) {
 
-        List<CategoryLevel> categoryLevels = categories.stream()
+    public static List<CategoryLevel> buildCategoryLevel(List<Category> categories) {
+        return buildCategoryLevel(
+                categories,
+                CollectionUtils.isNotEmpty(categories) ? (
+                        categories.stream().map(Category::getParentId).distinct().sorted().toList().get(0)
+                ) : 0
+        );
+    }
+
+    private static List<CategoryLevel> buildCategoryLevel(List<Category> categories, long parentId) {
+
+        List<CategoryLevel> categoryLevels = new ArrayList<>();
+
+        if (CollectionUtils.isNotEmpty(categories)) categories.stream()
                 .filter(c -> parentId == c.getParentId())
                 .sorted(Comparator.comparing(Category::getSequence))
                 .map(CategoryLevel::new)
